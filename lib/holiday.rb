89585326690a729048e036f8487ac9a1ba556a87
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-17 16:31:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,8 @@ end
 #not yet passing
 def all_winter_holiday_supplies(holiday_hash)
    supply_list = []
-   holiday_hash[:winter].each do |holiday_hash|
+   holiday_hash.each do |season, holiday_hash|
+     if season == :winter
      holiday_hash.each do |holiday, supply_array|
        binding.pry
          supply_array.each do |supply|
