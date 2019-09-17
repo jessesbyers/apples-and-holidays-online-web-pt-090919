40a6bb92b1ff942b6479b64b682f524f23f2fc93
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-17 16:13:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def all_winter_holiday_supplies(holiday_hash)
    holiday_hash[:winter].each do |holiday_hash|
        holiday_hash.each do |holiday, supply_array|
          supply_list << supply_array
-         supply_list.flatten
+         supply_list.flatten.to_s
 #         binding.pry
     end
   end
