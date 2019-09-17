8a03f7e571eda4b7cf7dae2e92ade4829150c0c9
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-17 18:06:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
         if supply_array.length == 2
 
       puts "  #{holiday.to_s.gsub("_"," ").capitalize}: #{supply_array[0]}, #{supply_array[1]}"
-
+binding.pry
     else
 
       puts "  #{holiday.to_s.gsub("_"," ").capitalize}: #{supply_array[0]}"
