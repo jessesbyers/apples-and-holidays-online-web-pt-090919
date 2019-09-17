83c4d9b0532fa3abab40b61ae3dce24c2580120e
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-17 15:52:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,6 @@ end
 #Not passing - asks me to add valentines day, then says it should not have valentines day!!!
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
-  holiday_hash
 end
 
 
