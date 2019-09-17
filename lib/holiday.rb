e7a421ccf1a08522d6e321a481a242dad854417f
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-17 17:25:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,13 +54,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_hsh|
     puts "#{season.capitalize.to_s}:"
     holiday_hsh.each do |holiday, supply_array|
-      supply_array.each do |supply|
         if supply_array.length == 2
-      puts "  #{holiday.to_s.sub("_"," ").split.capitalize}: #{supply_array[0]}, #{supply_array[1]}"
+      puts "  #{holiday.to_s.gsub("_"," ").capitalize}: #{supply_array[0]}, #{supply_array[1]}"
     else
-      puts "  #{holiday.to_s.sub("_"," ").split.capitalize}: #{supply_array[0]}"
+      puts "  #{holiday.to_s.gsub("_"," ").capitalize}: #{supply_array[0]}"
       end
-        end
       end
     end
   end
